9cd1cb6ed58631c2e6f8c55b09bd39e7f58f0697
Jolita <jolita278@gmail.com>
2017-06-28 16:31:41 +0300

    created routes for HCPriceRules service

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'rules'], function () {
+    Route::get('/', ['as' => 'app.rules.index', 'uses' => 'HCPriceRulesController@index']);
+    Route::get('/create', ['as' => 'app.rules.create', 'uses' => 'HCPriceRulesController@create']);
+    Route::post('/create', ['uses' => 'HCPriceRulesController@store']);
+
+    Route::group(['prefix' => '{id}'], function () {
+        Route::get('/', ['uses' => 'HCPriceRulesController@show']);
+        Route::get('/edit', ['as' => 'app.rules.edit', 'uses' => 'HCPriceRulesController@edit']);
+        Route::post('/edit', ['uses' => 'HCPriceRulesController@update']);
+        Route::delete('/', ['as' => 'app.rules.showDelete', 'uses' => 'HCPriceRulesController@destroy']);
+    });
+});
