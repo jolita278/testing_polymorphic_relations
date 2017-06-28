04f30122c7acaea99fc608abb83dae8302a5c633
Jolita <jolita278@gmail.com>
2017-06-28 15:03:18 +0300

    created HCPriceRulesController

---
+++ app/Http/Controllers/HCPriceRulesController.php
@@ -0,0 +1,90 @@
+<?php namespace App\Http\Controllers;
+
+use App\models\HCPriceRules;
+use Illuminate\Routing\Controller;
+
+class HCPriceRulesController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 * GET /hcpricerules
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /hcpricerules/create
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /hcpricerules
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+        //
+    }
+	
+
+	/**
+	 * Display the specified resource.
+	 * GET /hcpricerules/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 * GET /hcpricerules/{id}/edit
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /hcpricerules/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /hcpricerules/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
